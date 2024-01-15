d7c1cb65e7e5633e29ad42e0aa7c28b5b3721d82
kamdin84 <u2238149@warwick.ac.uk>
2024-01-15 20:32:56 +0000

    Webpage built, Updated Scaffolding

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using ccse_cw1.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using ccse_cw1.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
     options.UseSqlServer(connectionString);
 }); 
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
