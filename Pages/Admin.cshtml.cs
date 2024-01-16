b61cfd2ad7bc65dcf476fd594a15cc8109a6b8a2
kamdin84 <u2238149@warwick.ac.uk>
2024-01-16 00:32:33 +0000

    User Authorisation Added

---
+++ Pages/Admin.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ccse_cw1.Pages
+{
+    [Authorize(Roles = "admin")]
+    public class AdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
